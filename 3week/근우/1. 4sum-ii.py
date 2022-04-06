4b102f9a100ef69aac0fd0a9779e2e5b5b969dff
geunwoo Baek <geunu3751@nexon.co.kr>
2022-04-06 15:46:27 +0900

    solve 1,2

---
+++ 3week/근우/1. 4sum-ii.py
@@ -0,0 +1,18 @@
+class Solution:
+    def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
+        #1 <= n <= 200
+        # [nums1,nums2] [nums3,num4] 분할정복
+        ans = 0
+        nums_1_2_counter = {}
+        nums_3_4_counter = {}
+        for n1 in nums1:
+            for n2 in nums2:
+               nums_1_2_counter[n1+n2] = nums_1_2_counter.get(n1+n2,0)  + 1
+        for n3 in nums3:
+           for n4 in nums4:
+              nums_3_4_counter[n3+n4] = nums_3_4_counter.get(n3+n4,0)  + 1
+        ans = 0
+        for k,v in nums_1_2_counter.items():
+            ans += v*nums_3_4_counter.get(-k,0)
+        return ans
+
---
+++ 3week/근우/2. coin-change.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        NOT_VISIT = 2e9
+        VISIT_BUT_NOT_FOUND = 2e9 + 1
+        coins = list(filter(lambda x : x<=amount,coins))
+        coins.sort(reverse=True)
+        cache = [NOT_VISIT for _ in range(amount+1)]
+        def _dp(cur):
+            if cache[cur]!=NOT_VISIT:
+                return cache[cur]
+            elif cur == amount:
+                return 0
+            res = VISIT_BUT_NOT_FOUND
+            for c in coins:
+                next_cur = cur+c
+                if next_cur <= amount:
+                    res = min(res, 1+ _dp(next_cur))
+            cache[cur] = res
+            return res
+        res = _dp(0)
+        return res if res!=VISIT_BUT_NOT_FOUND else -1
