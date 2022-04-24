4da222ef99d58c452a4c6f7c562d7188ee7f032e
KIM NAMGUE <knq512412@gmail.com>
2022-04-24 12:16:39 +0900

    Update 1. Shortest Path in a Grid with Obstacles Elimination.cpp

--- 5week/남규/1. Shortest Path in a Grid with Obstacles Elimination.cpp
+++ 5week/남규/1. Shortest Path in a Grid with Obstacles Elimination.cpp
@@ -55,4 +55,70 @@ public:
 };
 
 
+// Accepted
 
+/*
+
+Approach
+1. cache N*M, A value of (i, j) is current remaining K
+2. BFS (All elements inside of queue have a same step)
+3. if (i, i) is (N - 1, M - 1), return step immedietely
+
+Time Complexity: O(NMK)
+Space Complexity: O(NMK) for Queue
+
+*/
+
+class Solution {
+public:
+    int shortestPath(vector<vector<int>>& grid, int k) {
+        int N = grid.size();
+        int M = grid[0].size();
+        
+        int cache[41][41];
+        memset(cache, -1, sizeof(cache));
+        queue<vector<int>> que;
+        que.push({0, 0, k});
+        cache[0][0] = k;
+        vector<vector<int>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        int step = 0;
+        
+        while(!que.empty()){
+            int len = que.size();
+            for(int i = 0; i < len; i++){
+                int y = que.front()[0];
+                int x = que.front()[1];
+                int currK = que.front()[2];
+                
+                if((y == N - 1) && (x == M - 1)){
+                    return step;
+                }
+                
+                que.pop();
+                
+                for(auto direction : directions){
+                    int nextY = y + direction[0];
+                    int nextX = x + direction[1];
+                    
+                    if(nextY >= N || nextY < 0 || nextX >= M || nextX < 0){
+                        continue;
+                    }
+                    
+                    if(grid[nextY][nextX] == 1){
+                        if(cache[nextY][nextX] < currK - 1){
+                            cache[nextY][nextX] = currK - 1;
+                            que.push({nextY, nextX, currK - 1});
+                        }
+                    } else if(grid[nextY][nextX] == 0){
+                        if(cache[nextY][nextX] < currK){
+                            cache[nextY][nextX] = currK;
+                            que.push({nextY, nextX, currK});
+                        }
+                    }
+                }
+            }
+            step++;
+        }
+        return -1;
+    }
+};
