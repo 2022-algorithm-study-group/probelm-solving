57d1f11542e948e7199f14221ccc8791643a10ea
Jaden Heo <hhj801@gmail.com>
2022-05-07 04:02:03 +0900

    5week commit

--- 5week/홍준/5.remove-all-ones-with-row-and-column-flips.py
+++ 5week/홍준/5.remove-all-ones-with-row-and-column-flips.py
@@ -1,1 +1,12 @@
 ## https://leetcode.com/problems/remove-all-ones-with-row-and-column-flips/
+
+from typing import List
+
+class Solution:
+    def removeOnes(self, grid: List[List[int]]) -> bool:
+        base = grid[0]
+        opposite = [1-i for i in base]
+        for row in grid:
+            if row != base and row != opposite:
+                return False
+        return True
